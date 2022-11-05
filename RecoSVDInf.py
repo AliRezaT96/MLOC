2a10f1ad89e7fa61f27fad7a1421e526f984cfbe
AliRezaT96 <alireza.toghroli1996@gmail.com>
2022-11-05 15:07:15 +0330

    SVD

---
+++ RecoSVDInf.py
@@ -0,0 +1,35 @@
+from surprise import SVD, Dataset
+from surprise.dump import load
+from collections import defaultdict
+import pandas as pd
+
+def get_top_n(predictions, n=10):
+    """Return the top-N recommendation for each user from a set of predictions.
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    """
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+
+df = pd.read_csv('./movies.dat',sep="::",header=None,engine='python')
+df.columns = ['iid','name','genre']
+df.set_index('iid',inplace=True)
+predictions, algo = load('./surprise_model')
+
+
